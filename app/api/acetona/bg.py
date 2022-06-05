7a7466c5ce7bd02ee514ccb4721bb9589f0855e2
matheusfrancisco <matheusmachadoufsc@gmail.com>
2022-06-05 17:08:50 -0300

    fix namespace

--- app/api/acetona/bg.py
+++ app/api/acetona/bg.py
@@ -8,7 +8,7 @@ from PIL.Image import Image as PILImage
 from pymatting.alpha.estimate_alpha_cf import estimate_alpha_cf
 from pymatting.foreground.estimate_foreground_ml import estimate_foreground_ml
 from pymatting.util.util import stack_images
-from scipy.ndimage.morphology import binary_erosion
+from scipy.ndimage import binary_erosion
 
 from app.api.acetona.session_base import BaseSession
 from app.api.acetona.session_factory import new_session
